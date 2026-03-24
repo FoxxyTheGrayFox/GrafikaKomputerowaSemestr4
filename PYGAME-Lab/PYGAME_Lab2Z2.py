7ada4a13ed94317c70533f75097454506f8c60cd
JustAFoxxyFox <foxxythefoxowo@gmail.com>
2026-03-24 18:47:01 +0100

    Uproszczenia
    Kolory nieuzywane w figurze usuniete

--- PYGAME-Lab/PYGAME_Lab2Z2.py
+++ PYGAME-Lab/PYGAME_Lab2Z2.py
@@ -4,15 +4,7 @@ pygame.init()
 win = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Zadanie 2")
 
-CZERWONY = (255, 0, 0)
-ZIELONY = (0, 255, 0)
-ZOLTY = (255, 255, 0)
-FIOLETOWY = (128, 0, 128)
-JASNY_NIEBIESKI = (0, 255, 255)
-POMARANCZOWY = (255, 165, 0)
 NIEBIESKI = (0, 0, 255)
-SZARY = (128, 128, 128)
-CZARNY = (0, 0, 0)
 BIALY = (255,255,255)
 
 run = True
